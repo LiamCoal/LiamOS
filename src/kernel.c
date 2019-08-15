6b87719573d2d89079d8f852eb1d40db09b79935
liamiam <liam@liamiam.com>
2019-08-15 01:02:59 -0700

    Make cool graphic thingy

--- src/kernel.c
+++ src/kernel.c
@@ -7,14 +7,15 @@ unsigned char *memory = (unsigned char*)0x00500;
 unsigned char *vgamem = (unsigned char*)0xA0000;
 int cur = 0;
 const char 
-black = 0x00,
-white = 0x0F,
-blue = 0x01,
-green = 0x02,
-red = 0x04,
-lblue = 0x09,
-lgreen = 0x0A,
-lred = 0x0C;
+    black = 0x00,
+    white = 0x0F,
+    blue = 0x01,
+    green = 0x02,
+    red = 0x04,
+    lblue = 0x09,
+    lgreen = 0x0A,
+    lred = 0x0C,
+    gray = 0x07;
 
 inline void setpxl(int x, int y, char col) {
     vgamem[x + (y * 320)] = col;
@@ -35,7 +36,15 @@ inline void vmode(short v) {
     asm("int $0x10" : : "a" (v + (a << 8)));
 }
 
+inline void cls() {
+    drawrect(0, 0, 320, 200, black);
+}
+
 int main() {
     vmode(0x13);
-    drawrect(0, 0, 320, 200, lgreen);
+    drawrect(0, 0, 320, 200, gray);
+    drawrect(100, 40, 110, 110, white);
+    drawrect(120, 60, 50, 50, lgreen);
+    drawrect(130, 70, 50, 50, lblue);
+    drawrect(140, 80, 50, 50, lred);
 }
